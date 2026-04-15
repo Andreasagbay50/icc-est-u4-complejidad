72dc2f869f0a0c8991eeaba57ee03385eb80c119
Andrea <cristinasagbay5@gmail.comm>
2026-04-15 15:52:02 -0500

    Add estudents list

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 ## Getting Started
+# Práctica: O4.O1 Complejidad Proyecto JAVA
 
+## Datos del Estudiante
+- **Nombre:** Andrea Sagbay
+- **Curso:** grupo 3
+- **Fecha:** 
+
+---
+
+## 1. [Título de la sección] o [Practica]
+![Descripción de la captura](assets/02-captura-ejemplo.png)
+
+**Fecha:**14/03/2026
+
+**Descripción:** creamos el proyecto y subimos a github
+
+---
+
+## 2. icc-est-u4-complejidad
+![Descripción de la captura](assets/02-captura-ejemplo-2.png)
+**Fecha:** 15/03/26
+**Descripción:** Creamos la clase Estudiante y Generados y creamos un listado de estudiantes con datos aleatorios para buscar y optimizar la busqueda.
+
+---
+
+## 3. icc-est-u4-complejidad
+![Aplicación desplegada](assets/02-captura-demo.png)
+**Fecha:** 15/03/26
+**Descripción:** Ejemplos de bucles listados
 Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
 
 ## Folder Structure
@@ -19,5 +47,5 @@ The `JAVA PROJECTS` view allows you to manage your dependencies. More details ca
 
 Practica 04.01 fecha 14/4
 
-creamos el proyecto
+creamos el proyecto>
 
--- src/App.java
+++ src/App.java
@@ -1,18 +1,28 @@
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
-        int[] array = new int []{1,2,3,4,5};
-        System.out.println(array[0]);
-        System.out.println(array);
-        //For normal
-        for(int i=0; i<array.length; i++){
-            System.out.println(array[i]);
-        }
-        // FOR EACH
-        for (int i : array){
-            System.out.println(i);
-        }
+        int cantidadEstudiantes= 30_00000; 
+        int cantidad = 1_000_000; 
 
-    }
+        List<Estudiante> estudiantes = Generador.generarListado(cantidadEstudiantes); 
+
+        //for (Estudiante est : estudiantes) {
+          //  System.out.println(est.nombre);
+       // }
+        String nombreBuscar = "Estudiante_2900111";
+        int intentos= 0;
+        for(Estudiante est: estudiantes){
+            if(est.nombre . equals(nombreBuscar)){
+                System.out.println("EXISTE");
+                break;
+            } else {
+                intentos++;
+            }
+        }
+        System.out.println( "fin");
+        System.out.println(intentos);
+     }
+    
 }
